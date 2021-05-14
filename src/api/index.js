5b234c5c49ad62f44d853c627b9ac9c3a9cbdb86
quincyp <quincytpham@gmail.com>
2021-05-13 18:02:54 -0700

    Added line chart showing infected & death over time, recovered does not appear to have daily/over time data

--- src/api/index.js
+++ src/api/index.js
@@ -11,7 +11,21 @@ export const fetchData = async () => {
             lastUpdate,
         };
     } catch (err) {
-
+        console.log(err);
     }
 }
 
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
+    } catch (err) {
+        console.log(err);
+    }
+}
